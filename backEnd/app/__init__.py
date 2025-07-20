232cd22e5e47e7b9d7076df2a53731fcb08c3578
guilhermevini2013 <guilhermevini2013@hotmail.com>
2025-07-20 12:33:00 -0300

    feat: Cors configuration and verification media type implemented

--- backEnd/app/__init__.py
+++ backEnd/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
+from flask_cors import CORS
 from backEnd.app.controllers.VideoController import video_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins = ["http://localhost:5173"])
     app.register_blueprint(video_bp)
     return app
--- backEnd/app/controllers/VideoController.py
+++ backEnd/app/controllers/VideoController.py
@@ -15,9 +15,13 @@ face_service = FaceService(cv2_service, file_service)
 @video_bp.route("/blur", methods=["POST"])
 def blur_video():
     if 'file' not in request.files:
-        return Error_model("file not found.", 400).show()
+        return Error_model("Argument 'file' not found.", 400).show()
 
     video = request.files['file']
+
+    if not video.mimetype.startswith('video/'):
+        return Error_model("The uploaded file is not a valid video.", 400).show()
+
     video_bytes = video.read()
     video_bytes_with_blur = face_service.blur_video(video_bytes, algorithm=TypeAlgorithm(request.form['algorithm']))
     return Response(video_bytes_with_blur, mimetype="video/mp4")
